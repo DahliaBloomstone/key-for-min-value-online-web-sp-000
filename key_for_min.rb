59e5458b47360683bd9fbd52e8da6832ebf61340
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-05-21 20:53:57 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,6 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-
+# method called, passed an argument of an empty hash => nil
 def key_for_min_value(name_hash)
 
 end
